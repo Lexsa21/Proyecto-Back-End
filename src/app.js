788a64087337d2ad0af19b98d4948633309af8f9
Lexsa21 <estradaaxel021@gmail.com>
2024-06-05 23:11:54 -0300

    preentrega__coder

--- src/app.js
+++ src/app.js
@@ -1,9 +1,13 @@
 import express from "express"
-import { __dirname } from "./utils.js"
+import { SECRET, __dirname } from "./utils.js"
 import handlebars from "express-handlebars"
 import {Server} from "socket.io"
+import cookieParser from "cookie-parser"
+
 import routerP from "./routes/products.router.js";
 import routerV from "./routes/views.router.js";
+import routerC from "./routes/cookies.routes.js";
+
 import socketProducts from "./listeners/socketProducts.js";
 import socketChat from "./listeners/socketChat.js";
 import connectToDB from "./Dao/config/configServer.js";
@@ -14,6 +18,7 @@ const app = express()
 const PORT=3000
 
 app.use(express.static(__dirname + "/public"))
+app.use(cookieParser(SECRET))
 
 //handlebars
 app.engine("handlebars",handlebars.engine())
@@ -23,6 +28,7 @@ app.set("view engine","handlebars")
 //rutas
 app.use("/api",routerP)
 app.use('/', routerV);
+app.use('/api/cookies', routerC)
 
 connectToDB()
 
---
+++ src/routes/cookies.routes.js
@@ -0,0 +1,39 @@
+import { Router } from 'express';
+import { __dirname } from '../utils.js';
+
+
+const routerC = Router();
+
+routerC.get('/getcookies', async (req, res) => {
+    try {
+
+        const data = JSON.parse(req.signedCookies['codercookie']);
+        
+        res.status(200).send({  payload: data });
+    } catch (err) {
+        res.status(500).send({  payload: null, error: err.message });
+    }
+});
+
+routerC.get('/setcookie', async (req, res) => {
+    try {
+
+        const cookieData = { user: 'axel', email: 'idux.net@gmail.com' };
+        res.cookie('codercookie',JSON.stringify(cookieData) , { maxAge: 30000, signed: true });
+        
+        res.status(200).send({  payload: 'Cookie generada' });
+    } catch (err) {
+        res.status(500).send({  payload: null, error: err.message });
+    }
+});
+/*
+routerC.get('/deletecookie', async (req, res) => {
+    try {
+        res.clearCookie('codercookie');
+        res.status(200).send({ origin: config.SERVER, payload: 'Cookie eliminada' });
+    } catch (err) {
+        res.status(500).send({ origin: config.SERVER, payload: null, error: err.message });
+    }
+});
+*/
+export default routerC;
